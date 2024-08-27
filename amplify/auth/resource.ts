14ca0209f748d5cce1a121d947cb464b5105f46e
Kadoi Kensuke <kensuke-kadoi@primo.mamezou.com>
2024-08-27 18:32:24 +0900

    add auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,22 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from '@aws-amplify/backend';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      oidc: [
+        {
+          name: 'auh0',
+          clientId: secret('nkFhNHWBvXO0Rq5jpTZPjOLzzaf3XyUx'),
+          clientSecret: secret('6o3csq9JlFsKrS6GUzcO51QU3nRP9fDn6-8A6hdzJV6zdbNx3YbryG_gyz8r_dCI'),
+          issuerUrl: 'dev-n2xybsy8ikhganbx.us.auth0.com',
+        },
+      ],
+      logoutUrls: ['http://localhost:3000/', 'https://mywebsite.com'],
+      callbackUrls: [
+        'http://localhost:3000/profile',
+        'https://mywebsite.com/profile',
+      ],
+    },
   },
 });
