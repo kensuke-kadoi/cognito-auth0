7fb3c0cd43899a104c8e46f0c44d34105b6278a2
Kadoi Kensuke <kensuke-kadoi@primo.mamezou.com>
2024-08-27 19:37:03 +0900

    add auth settings

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from "@aws-amplify/backend";
 
 /**
  * Define and configure your auth resource
@@ -7,5 +7,41 @@ import { defineAuth } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      oidc: [
+        {
+          name: "auh0",
+          clientId: secret("AUTH0_CLIENT_ID"),
+          clientSecret: secret("AUTH0_CLIENT_SECRET"),
+          issuerUrl: "dev-n2xybsy8ikhganbx.us.auth0.com",
+          scopes: ["email", "opendid", "profile"],
+        },
+      ],
+      logoutUrls: ["http://localhost:3000/"],
+      callbackUrls: ["http://localhost:3000/profile"],
+    },
   },
 });
+
+// import { defineAuth, secret } from '@aws-amplify/backend';
+//
+// export const auth = defineAuth({
+//   loginWith: {
+//     email: true,
+//     externalProviders: {
+//       oidc: [
+//         {
+//           name: 'auh0',
+//           clientId: secret('nkFhNHWBvXO0Rq5jpTZPjOLzzaf3XyUx'),
+//           clientSecret: secret('6o3csq9JlFsKrS6GUzcO51QU3nRP9fDn6-8A6hdzJV6zdbNx3YbryG_gyz8r_dCI'),
+//           issuerUrl: 'dev-n2xybsy8ikhganbx.us.auth0.com',
+//         },
+//       ],
+//       logoutUrls: ['http://localhost:3000/', 'https://mywebsite.com'],
+//       callbackUrls: [
+//         'http://localhost:3000/profile',
+//         'https://mywebsite.com/profile',
+//       ],
+//     },
+//   },
+// });
